03c4c9f363eab7afaf5a707b69694cece5fdbb10
mef51 <hms.mohammed@gmail.com>
2015-06-16 20:08:03 -0400

    add mediteranean regions data

--- static/game.js
+++ static/game.js
@@ -2,10 +2,11 @@ var canvas = document.getElementById('viewport'),
 	context = canvas.getContext('2d');
 
 var rect = canvas.getBoundingClientRect();
-canvas.addEventListener('mousemove', function(e){
+canvas.addEventListener('click', function(e){
 	var x = Math.round((e.clientX-rect.left)/(rect.right-rect.left)*canvas.width);
 	var y = Math.round((e.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height);
-	console.log(x, y);
+	var region = prompt("Please enter the region name");
+	console.log(region, ':', x, ',', y);
 });
 
 // colors
